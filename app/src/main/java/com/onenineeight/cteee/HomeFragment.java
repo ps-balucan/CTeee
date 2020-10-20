31f2e39b42f5189010b2d06954e33549d4935002
Andy <andrew.concejero@gmail.com>
2020-10-20 20:15:28 +0800

    Temp homepage Risk Front end edit
    Changes homepage to color red and "At Risk" - Requires additional if statements to handle if user truly is at risk

--- app/src/main/java/com/onenineeight/cteee/HomeFragment.java
+++ app/src/main/java/com/onenineeight/cteee/HomeFragment.java
@@ -1,18 +1,31 @@
 package com.onenineeight.cteee;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        CardView card = view.findViewById(R.id.safeCard);
+        card.setCardBackgroundColor(Color.RED);
+
+        TextView safe = view.findViewById(R.id.safeOrAtRisk);
+        safe.setText("At Risk");
+
+        return view;
     }
 }

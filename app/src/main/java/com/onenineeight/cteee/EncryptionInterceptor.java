066db887ae1db6a4934cbdbce7d63669b278ede7
Andy <andrew.concejero@gmail.com>
2020-11-25 15:00:14 +0800

    Asymetric Encryption v1
    Added encryption decryption of asymetric

--- app/src/main/java/com/onenineeight/cteee/EncryptionInterceptor.java
+++ app/src/main/java/com/onenineeight/cteee/EncryptionInterceptor.java
@@ -17,11 +17,18 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
@@ -45,6 +52,11 @@ public class EncryptionInterceptor implements Interceptor {
     public static final int GCM_TAG_LENGTH = 16;
     private static String appname = "Pr1v4Tr4c3#!";
     private static String salt = "pepper is better";
+    private static String publicKeyString = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCutGgtBr00iIAEuH7pRpNhE3AjPkiaOvMhl5o8KeQ2nG8HiEYYUYi00vTeFACozb7rPjFM+bI6pdXt2EITeXWhse0eHS1u08G5eHlhmoyNK7rx009TlsAW55TRLtNRY2QyO6jV3VhEBv8mxl47MXMIlcr/yrRSLs9UkZWUIU0a0QIDAQAB";
+
+    static String privateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAK60aC0GvTSIgAS4fulGk2ETcCM+SJo68yGXmjwp5DacbweIRhhRiLTS9N4UAKjNvus+MUz5sjql1e3YQhN5daGx7R4dLW7Twbl4eWGajI0ruvHTT1OWwBbnlNEu01FjZDI7qNXdWEQG/ybGXjsxcwiVyv/KtFIuz1SRlZQhTRrRAgMBAAECgYAC8RqhOfgjB6KkubTY4aFrEL5KWKLVNoWIMPbl2RR8wy33MfEwuQRA273H9M0lSogiytJ+k+SPemIHohTsQicD97PLLLyq2x35r/rSfWpnhkOew1LILokde5zjKmaxiO2qpZHhax5Nra5x6urGGEpqKcAcFLv3w+RCCbNeheaiYQJBANw70PXYpuhsbAs8BJa4nOX+hUNR/9c7JC8THFkG8eNSS2G2Wu2PqzthZLzIz+72kacKuortfLFeViljnJPcwLECQQDLE7swrQMB1J2ZbcucwOp6N06bbtWuuBvqg1pEwk+6BGaWFn9JVTT6CnHZZ3wmZn7eVV5h2fFupcUpe1ItjYQhAkEAzkHvJ/BqEQ9J+ROSsJ3hw70ljbEETM0EzFm3mOFaNS3jj3W7nvWIxqomMHBZ3VFrg+gWYPqeZ3cUdtrlz81VYQJAA68u9ToeVNkrps30JLrnRiJcvbssC0hx0TZi295+ncxYpB5yXr06AdNQr3YOLZAsexInbTUMCUreodsH6aWooQJAHUZlfcxlk+Qdx3D3oWDlgZRa5NFXhWknLxIQ5Ggb3VgSTrEIYmdaH1+TM4qnGikS/pI4rH9nJdLyAqZDgeMM+A==";
+    static String encryptedTextString = "<your_received_encrypted_text_here>";
+    static String encryptedSecretKeyString = "Ilt6eaVG8E6m6bpn0pl1DubDyjC42a8FypFbkZBAuHs8Iw2Lmk2aoHslzzb1mz0bjsYF7HDd32JMNokPoEBF+OwO1IgiUy412aB9lRWicCsek8FQP90FjyaJz5ipmTgweaOeShYg0UKTFilDYQrxGG0MY5oJP9fAUJlgfd9EPZE=";
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -74,8 +86,8 @@ public class EncryptionInterceptor implements Interceptor {
             String[] tokens = text.split(":");
 
             //parsing the username only
-            //for (String t : tokens)
-               // System.out.println(t);
+            for (String t : tokens)
+                System.out.println(t);
             String parsed = tokens[1].substring(1, tokens[1].length() - 2);
 
             //System.out.println("The username only: " + parsed);
@@ -85,16 +97,41 @@ public class EncryptionInterceptor implements Interceptor {
             KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
             keyGenerator.init(256);
 
-            SecretKey secretKey1 = keyGenerator.generateKey();
+            SecretKey secretKey = keyGenerator.generateKey();
             //secretKey = javax.crypto.spec.SecretKeySpec@178f0;
 
+            /*
+            //depracated hard coded key
             //Generate key using password and salt
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             // iterationCount = 65536
             // keyLength = 256
             KeySpec spec = new PBEKeySpec(appname.toCharArray(), salt.getBytes(), 65536, 256);
             SecretKey secretKey = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+            //end of hard coded key
+            */
             System.out.println("secret key: " + secretKey);
+
+            /*RSA KEY GIVING THING
+            try {
+
+                // 1. generate public key and private key
+                KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+                keyPairGenerator.initialize(1024); // key length
+                KeyPair keyPair = keyPairGenerator.genKeyPair();
+                String privateKeyString = Base64.getEncoder().encodeToString(keyPair.getPrivate().getEncoded());
+                String publicKeyString = Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded());
+
+                // 2. print both keys
+                System.out.println("rsa key pair generated\n");
+                System.out.println("privateKey\n" + privateKeyString + "\n");
+                System.out.println("publicKey\n" + publicKeyString + "\n\n");
+
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            */
+            //END OF RSA
 
 
             byte[] IV = new byte[12];
@@ -113,7 +150,7 @@ public class EncryptionInterceptor implements Interceptor {
             IV[9] = -33;
             IV[10] = -17;
             IV[11] = 51;
-            System.out.println("IV: " + strbruh);
+            /*System.out.println("IV: " + strbruh);
             System.out.println("IV: " + IV);
             System.out.println("IV0: " + IV[0]);
             System.out.println("IV1: " + IV[1]);
@@ -126,7 +163,7 @@ public class EncryptionInterceptor implements Interceptor {
             System.out.println("IV8: " + IV[8]);
             System.out.println("IV9: " + IV[9]);
             System.out.println("IV10: " + IV[10]);
-            System.out.println("IV11: " + IV[11]);
+            System.out.println("IV11: " + IV[11]);*/
 
             byte[] plaintext = text.getBytes();
             //System.out.println("The username only: " + plaintext);
@@ -147,13 +184,40 @@ public class EncryptionInterceptor implements Interceptor {
             //Step 3. Print encrypted text for validation
             String str2 = Base64.getEncoder().encodeToString(cipherText);
             System.out.println("Encrypted Text : " + str2);
-            String resultString = tokens[0] + ":\"" + cipherText + "\"}";
-           //System.out.println("Final Text : " + resultString);
 
+            //String resultString = tokens[0] + ":\"" + str2 + "\",\"logs\":" + tokens[2];
+            //System.out.println("Final Text : " + resultString);
+
+            //Step 3.5. Encrypt the encryption key using RSA Public Key
+            X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKeyString));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey publicKey = keyFactory.generatePublic(publicSpec);
+
+            Cipher cipher2 = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+            cipher2.init(Cipher.ENCRYPT_MODE, publicKey);
+            String encryptedSecretKey = Base64.getEncoder().encodeToString(cipher2.doFinal(secretKey.getEncoded()));
+            System.out.println("encrypted secret key : " + encryptedSecretKey);
+
+            String resultString = tokens[0] + ":\"" + str2 + "\",\"logs\":" + encryptedSecretKey + "}";
+            System.out.println("resultString : " + resultString);
             //STEP 4. DECRYPT
             try {
+
+                // 1. Get private key
+                PKCS8EncodedKeySpec privateSpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey));
+                KeyFactory keyFactory1 = KeyFactory.getInstance("RSA");
+                PrivateKey privateKey = keyFactory1.generatePrivate(privateSpec);
+
+                // 2. Decrypt encrypted secret key using private key
+                Cipher cipher1 = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+                cipher1.init(Cipher.DECRYPT_MODE, privateKey);
+                byte[] secretKeyBytes = cipher1.doFinal(Base64.getDecoder().decode(encryptedSecretKey));
+                SecretKey secretKey1 = new SecretKeySpec(secretKeyBytes, 0, secretKeyBytes.length, "AES");
+
+
+                // 3. decrypt encrypted messaging using the just decrypted secret key
                 Cipher dcipher = Cipher.getInstance("AES/GCM/NoPadding");
-                SecretKeySpec dkeySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
+                SecretKeySpec dkeySpec = new SecretKeySpec(secretKey1.getEncoded(), "AES");
                 GCMParameterSpec dgcmParameterSpec = new GCMParameterSpec(16 * 8, IV);
                 IvParameterSpec divSpec = new IvParameterSpec(IV);
                 dcipher.init(Cipher.DECRYPT_MODE, dkeySpec, dgcmParameterSpec);
@@ -164,11 +228,14 @@ public class EncryptionInterceptor implements Interceptor {
                 System.out.println("decrypted Text : " + str3);
                 //String dresultString = tokens[0] + ":\"" + decryptedText + "\"}";
                 //System.out.println("Final Decrypted Text : " + dresultString);
+
+
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            return str2;
+            return resultString;
 
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException | InvalidKeySpecException e) {
             System.err.println("I'm sorry, but MD5 is not a valid message digest algorithm");
